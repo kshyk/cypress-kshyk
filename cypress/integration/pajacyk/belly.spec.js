bfe18f0037bc85c3e43ed65d850dee183e60cf17
kshyk <jj.turkiewicz@gmail.com>
2020-10-12 19:30:09 +0200

    - removed examples

--- cypress/integration/pajacyk/belly.spec.js
+++ cypress/integration/pajacyk/belly.spec.js
@@ -1,16 +1,9 @@
 /// <reference types="cypress" />
 
 context('Belly', () => {
-    beforeEach(() => {
-        cy.visit('https://www.pajacyk.pl/#index')
-    })
-
-    it('should contains Pajacyk in the page title', () => {
-        cy.title().should('contain', 'Pajacyk')
-    });
-
     it('should print thanks message after belly click', () => {
-        cy.get('paj-click').click()
+        cy.visit('https://www.pajacyk.pl/#index')
+        cy.get('.paj-click').click()
         cy.document().should('contain.text', 'dziękujemy :)')
     });
 });
