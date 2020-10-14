f3e510ea5dce83f4551280be9a93da68f8c1f4cd
kshyk <jj.turkiewicz@gmail.com>
2020-10-14 21:33:48 +0200

    - added cookies test specs for T&P

---
+++ cypress/integration/teamandpersonal/cookies.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+context('Cookies Bar', () => {
+    beforeEach(() => {
+        cy.visit('https://teamandpersonal.pl/')
+    })
+
+    it('should be present on page load', () => {
+        cy.get('div#catapult-cookie-bar')
+          .should('be.visible')
+          .should('contain.text', 'Serwis wykorzystuje pliki cookies. Korzystając ze strony wyrażasz zgodę na wykorzystywanie plików cookies. dowiedz się więcej.')      
+    });
+
+    it('should hide when OK button is clicked', () => {
+        cy.get('button[id=catapultCookie]').click()
+        cy.get('div#catapult-cookie-bar').should('not.exist')
+    });
+});
