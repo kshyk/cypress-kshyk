0ac84103ad6e6ac4c7e6a346594e3766f27d69c7
kshyk <jj.turkiewicz@gmail.com>
2020-10-17 01:06:53 +0200

    - CM startup test renamed + status code check

--- cypress/integration/crazymountains/startup.spec.js
+++ cypress/integration/crazymountains/startup.spec.js
@@ -1,10 +1,14 @@
 /// <reference types="cypress" />
 
 context('Crazy Mountains', () => {
-    it('should be site under construction', () => {
-        cy.request('GET', 'http://crazymountains.com/')
-            .then((response) => {
-                expect(response.status).to.eq(503)
-            })
+    it('should be under construction', () => {
+        cy.request({
+          method: 'GET',
+          url: 'http://crazymountains.com/',
+          failOnStatusCode: false
+        })
+        .then((response) =>
+          expect(response.status).to.eq(503)
+        )
     });
 });
