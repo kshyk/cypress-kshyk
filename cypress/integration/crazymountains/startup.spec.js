eaf5828a3efbd524439a2f32f7d1bb60832c4549
kshyk <jj.turkiewicz@gmail.com>
2020-10-17 00:07:59 +0200

    - startup testfix

--- cypress/integration/crazymountains/startup.spec.js
+++ cypress/integration/crazymountains/startup.spec.js
@@ -7,8 +7,9 @@ context('Crazy Mountains', () => {
 
     it('should be site under construction', () => {
         cy.get('h1').should('have.text', 'Tryb konserwacji')
-        cy.get('h2 > p').should('have.text','Przepraszamy za niedogodności.
-Nasza strona jest obecnie poddawana pracom konserwacyjnym.
-Dziękujemy za zrozumienie.')
+        cy.get('h2 > p')
+          .should('contain.text', 'Przepraszamy za niedogodności.')
+          .should('contain.text', 'Nasza strona jest obecnie poddawana pracom konserwacyjnym.')
+          .should('contain.text', 'Dziękujemy za zrozumienie.')
     });
 });
