1f4030b57c2947f7fbfba69b7a9d771b06367577
kshyk <jj.turkiewicz@gmail.com>
2021-03-06 23:53:56 +0100

    feat: upload html report + video recordings

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,11 +15,18 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands"
+import addContext from "ochawesome/addContext"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 Cypress.on("uncaught:exception", (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
+    // returning false here prevents Cypress from failing the test
     return false;
+});
+
+Cypress.on("test:after:run", (test, runnable) => {
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+    addContext({ test }, videoUrl)
 });
