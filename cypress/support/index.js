079327130543e5126617943e0c2de79d4bfdd0d9
kshyk <jj.turkiewicz@gmail.com>
2021-03-07 00:06:41 +0100

    fix: typo

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import "./commands"
-import addContext from "ochawesome/addContext"
+import addContext from "mochawesome/addContext"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
