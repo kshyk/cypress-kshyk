e040bb4473102434e7db4e32e3a862b98ad9c42b
kshyk <jj.turkiewicz@gmail.com>
2021-03-06 22:59:38 +0100

    fix: removed http protocol

--- cypress/integration/okruszek/breadcrumb.spec.js
+++ cypress/integration/okruszek/breadcrumb.spec.js
@@ -2,7 +2,7 @@
 
 context("Breadcrumb", () => {
     it("should donate after bread click", () => {
-        cy.visit("http://www.okruszek.org.pl/");
+        cy.visit("www.okruszek.org.pl");
         cy.get(".click-crumb").click();
         cy.document().should("contain.text", "DZIĘKUJEMY!");
     });
