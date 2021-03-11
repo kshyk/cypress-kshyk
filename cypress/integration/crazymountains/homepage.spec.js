303fec917b30d9d7e01b05c58db6a32a977e0d9b
kshyk <jj.turkiewicz@gmail.com>
2021-03-12 00:20:50 +0100

    refactor: social media icons test: of footer data

---
+++ cypress/integration/crazymountains/homepage.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+context("Home Page", () => {
+    before(() => {
+      cy.visit("https://crazymountains.eu/")
+    });
+
+    it("verifies social media icons in header", () => {
+      var medias = ["Facebook", "Instagram", "E-mail", "Pinterest", "YouTube"];
+      medias.forEach((item) => {
+        cy.get("[data-label='" + item + "']").should("be.visible");
+      });
+    });
+
+    it("verifies footer icons and data", () => {
+      cy.get(".payment-icon").should(($el) => {
+        expect($el).to.have.length(3)
+      });
+      cy.get(".copyright-footer")
+        .should("contain.text", "Copyright 2021 © crazymountains");
+    });
+});
